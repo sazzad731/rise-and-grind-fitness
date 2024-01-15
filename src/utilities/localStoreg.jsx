521fe6b163084c3c7a8aa577fe362fb978e0eb7f
sazzad <sazzad@DESKTOP-1UFS7DJ>
2024-01-15 21:45:57 +0600

    deleted unused function

--- src/utilities/localStoreg.jsx
+++ src/utilities/localStoreg.jsx
@@ -2,8 +2,4 @@ const addBreakToLocal = (time)=>{
     localStorage.setItem("Break-time", time)
 }
 
-const getBrakFromLocal = ()=>{
-    localStorage.getItem("Break-time");
-}
-
 export {addBreakToLocal}
